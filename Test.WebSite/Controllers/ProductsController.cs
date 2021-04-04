6c29a262f92e37deffe299b924d970ac39a50940
Corvit Chr <corvit.christian@gmail.com>
2021-04-04 17:25:39 +0300

    added controllers for api

---
+++ Test.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Test.WebSite.Models;
+using Test.WebSite.Services;
+
+namespace Test.WebSite.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
