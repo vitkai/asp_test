d2fd19fbf1afc9f53623c7b9fbf4b349ae226e69
Corvit Chr <corvit.christian@gmail.com>
2021-04-05 00:17:21 +0300

    added components

--- Test.WebSite/Startup.cs
+++ Test.WebSite/Startup.cs
@@ -28,6 +28,7 @@ namespace Test.WebSite
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddServerSideBlazor();
             services.AddControllers();
             services.AddTransient<JsonFileProductService>();
         }
@@ -57,6 +58,7 @@ namespace Test.WebSite
             {
                 endpoints.MapRazorPages();
                 endpoints.MapControllers();
+                endpoints.MapBlazorHub();
                 //endpoints.MapGet("/products", (context) =>
                 //{
                 //    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
